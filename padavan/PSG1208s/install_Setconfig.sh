88296fb8b4e297048e75829cd6bb79cae710d5a4
Joelyi <37099326+Joelyi@users.noreply.github.com>
2022-08-12 16:42:58 +0800

    Update install_Setconfig.sh

--- padavan/PSG1208s/install_Setconfig.sh
+++ padavan/PSG1208s/install_Setconfig.sh
@@ -8,4 +8,4 @@ cp -f "${ROOTDIR}/${CONFIG_FILENAME}" "${DESTDIR}/trunk/configs/templates/PSG120
 cp -f "${ROOTDIR}/defaults.h" "${DESTDIR}/trunk/user/shared/defaults.h"
 cp -f "${ROOTDIR}/board.h" "${DESTDIR}/trunk/configs/boards/PSG1208/board.h"
 cp -f "${ROOTDIR}/board.mk" "${DESTDIR}/trunk/configs/boards/PSG1208/board.mk"
-cp -f "${ROOTDIR}/kernel-3.4.x.config" "${DESTDIR}/trunk/configs/boards/PHICOMM/PSG1208/kernel-3.4.x.config"
+cp -f "${ROOTDIR}/kernel-3.4.x.config" "${DESTDIR}/trunk/configs/boards/PSG1208/kernel-3.4.x.config"
